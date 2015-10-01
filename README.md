317ebe53df062d44f4a42327b9baf02f95a25fa1
Mattias Ljung <mljung02@gmail.com>
2015-10-01 13:48:40 -0600

    intial 2015 layout

---
+++ README.md
@@ -0,0 +1,8 @@
+This app is intended to scroll backwards and forwards in time in order
+to show baby names and design paradigms.
+
+Baby Name source:  http://www.ssa.gov/oact/babynames/top5names.html
+
+Name Link source: http://www.famousbirthdays.com/names/derek.html
+
+Every era change the font changes, the nav bar changes, the background changes.
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,59 @@
-var window = document.getElementById('button1')
+var state = 2014;
+console.log('wired');
+console.log(names[state][0][0]);
+var nameShow = document.getElementById('nameshow');
+var stateshowboys = document.getElementById('stateshowboys');
+var stateshowgirls = document.getElementById('stateshowgirls');
+var timeout
+
+window.onload = populateNames
+
+function populateNames() {
+  stateshowboys.innerHTML = '';
+  stateshowgirls.innerHTML = '';
+  nameShow.innerHTML = ''
+  var h1 = document.createElement('h1');
+  h1.innerHTML = 'Top Baby Names for ' + state;
+  nameShow.appendChild(h1);
+  var olGirls = document.createElement('ol');
+  var olBoys = document.createElement('ol');
+  names[state][0].forEach(function (name) {
+    var li = document.createElement('li');
+    var a = document.createElement('a');
+    a.href= 'http://www.famousbirthdays.com/names/'+name.toLowerCase()+'.html';
+    a.target = '_blank';
+    a.innerHTML = name;
+    li.appendChild(a);
+    olGirls.appendChild(li);
+  })
+  stateshowgirls.appendChild(olGirls)
+  names[state][1].forEach(function (name) {
+    var li = document.createElement('li');
+    var a = document.createElement('a');
+    a.href= 'http://www.famousbirthdays.com/names/'+name.toLowerCase()+'.html';
+    a.target = '_blank';
+    a.innerHTML = name;
+    li.appendChild(a);
+    olBoys.appendChild(li);
+  })
+  stateshowboys.appendChild(olBoys);
+}
+
+function stateSwitch (x){
+  if (x < 0 && state > 1980) {
+    state--
+  } else if (x > 0 && state < 2014){
+    state++
+  }
+  console.log('state changed to :', state)
+}
 
 document.body.addEventListener('wheel', function (e) {
-  console.log(e.wheelDelta, 'yo');
+  console.log('yo', e.wheelDelta)
+  clearTimeout(timeout)
+  timeout = setTimeout(function(){
+    stateSwitch(e.wheelDelta)
+    console.log(state)
+    populateNames()
+  }, 40)
 })
